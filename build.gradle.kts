d3d67425dfa038a38cfdccbe21abe9ba827fca81
Michi Palazzo <palazzomichi@gmail.com>
2022-02-27 16:28:07 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm")
-    kotlin("kapt")
-    kotlin("plugin.serialization")
+    kotlin("jvm") version "1.6.10"
+    kotlin("kapt") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
 }
 
 group = "it.pureorigins"
@@ -17,6 +17,12 @@ dependencies {
     compileOnly("com.velocitypowered:velocity-api:3.0.0")
     compileOnly("com.github.PureOrigins:velocity-language-kotlin:1.0.0")
     compileOnly("com.github.PureOrigins:VelocityConfiguration:1.0.1")
-    compileOnly("com.github.PureOrigins:VelocityFriends:1.0.0")
-    kapt("com.velocitypowered:velocity-api:3.0.0")
+    compileOnly("com.github.PureOrigins:VelocityFriends:1.0.1")
+    kapt("com.velocitypowered:velocity-api:3.0.1")
+}
+
+kotlin {
+    jvmToolchain {
+        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(8))
+    }
 }
